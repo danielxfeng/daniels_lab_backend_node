4114217f7ac7c59d924e32a43b1be3ddd5d24762
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-04-18 19:55:13 +0300

    the 404 handler moved to routers.ts, error handler moved to middleware, the app.ts and routers were re-organized.

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,26 @@
 /**
  * @File app.ts
- * This is the main entry point of the application.
+ * This is the main entry point of the application,
+ * which defines the processing pipeline of the web server.
  *
  * 1. loads env.
  * 2. initializes express.
- * 3. loads middlewares.
- * 4. loads routes.
- * 5. handles 404 errors if no route is matched.
- * 6. handles other errors (via error handler middleware).
- * 7. starts the server and listens on the specified port.
+ * 3. loads pre-processing middlewares.
+ *   - to parse json.
+ *   - to parse urlencoded data.
+ *   - to host static files.
+ *   - ...
+ * 4. loads route, which is the main logic of the pipeline.
+ * 5. loads post-processing middlewares.
+ *   - error handler.
+ * 6. starts the server and listens on the specified port.
  */
 
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-import httpErrors from "http-errors";
 import routers from "./routes/routers";
+import errorHandler from "./middleware/error_handler";
 
 // Import env
 const envFile = `.env.${process.env.NODE_ENV || "development"}`;
@@ -24,7 +29,8 @@ dotenv.config({ path: path.resolve(__dirname, `../${envFile}`) });
 // Init express
 const app = express();
 
-// Load middlewares
+// - Pre-processing middlewares
+
 // to parse json
 app.use(express.json());
 // to parse urlencoded data
@@ -34,39 +40,23 @@ app.use(express.static(path.join(__dirname, "../public")));
 // todo: add more middlewares
 
 
-// Load routes
-app.use("/api", routers);
 
-// 404 handler
-// When no route is matched, will call this.
-app.use((req, res, next) => {
-  next(httpErrors(404, "Not Found"));
-});
+// The routers, as well as the main logic of the pipeline.
+app.use("/", routers);
 
-// Error handler
-// Log the error and send the response.
-// Will not send the internal error message in production.
-app.use(
-  (
-    err: any,
-    req: express.Request,
-    res: express.Response,
-    next: express.NextFunction
-  ) => {
-    const isProd = process.env.NODE_ENV === "production";
-    const isInternal = err.status >= 500 && err.status < 600;
 
-    console.error(err);
-    res.status(err.status || 500).json({
-      error: !isProd || !isInternal ? err.message : "Internal Server Error",
-    });
-  }
-);
+
+// - Post-processing middlewares
+
+// In Express 5, all the errors are passed to here.
+app.use(errorHandler);
+
+
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT: number = parseInt(process.env.PORT || "3000", 10);
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running at ${process.env.BASE_URL} port ${PORT}`);
   console.log(`Environment: ${process.env.NODE_ENV}`);
 });
 
