d069ec83a7f4c2ee3bb0607d6622622fa292cbcb
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-04-25 23:53:03 +0300

    applied middlewares to router_user, and fixed the controller.

--- src/routes/router_user.ts
+++ src/routes/router_user.ts
@@ -8,19 +8,27 @@
  */
 
 import { Router } from "express";
+import userController from "../controllers/controller_user";
+import { auth, authAdmin } from "../middleware/auth";
+import validate from "../middleware/validate";
+import { UpdateUserBodySchema } from "../schema/schema_users";
 
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => {
-  res.status(200).json({ message: "User profile to be implemented" });
-});
+userRouter.get("/",
+  auth,
+  userController.getCurrentUserProfile,
+);
 
-userRouter.put("/", (req, res) => {
-  res.status(200).json({ message: "Update user profile to be implemented" });
-});
+userRouter.put("/",
+  auth,
+  validate({ body: UpdateUserBodySchema }),
+  userController.updateCurrentUserInfo,
+);
 
-userRouter.get("/all", (req, res) => {
-  res.status(200).json({ message: "Admin: List users to be implemented" });
-});
+userRouter.get("/all",
+  authAdmin,
+  userController.listAllUsers,
+);
 
 export default userRouter;
